efd4dc53af0d14defa0b378cb89386e7c4f8db90
Joona Tolonen <54183803+tolonenj@users.noreply.github.com>
2023-08-16 13:32:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -e
 
 : ${GANESHA_CONFIGFILE:="/tmp/ganesha.conf"}
+: ${GANESHA_LOGFILE:="/tmp/ganesha.log"}
 
 function bootstrap_config {
         echo "Bootstrapping Ganesha NFS config"
@@ -56,18 +57,18 @@ LOG {
 
                 Facility {
                         name = FILE;
-                        destination = "/tmp/ganesha.log";
+                        destination = ${GANESHA_LOGFILE};
                         enable = active;
                 }
 }
 
-VFS
-{
-                link_support = false;
-                symlink_support = false;
-                umask = 0777;
-                only_one_user = true;
-}
+#VFS
+#{
+#                link_support = false;
+#                symlink_support = false;
+#                umask = 0777;
+#                only_one_user = true;
+#}
 
 END
 }
@@ -76,4 +77,4 @@ bootstrap_config
 
 echo "Starting Ganesha NFS"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
-exec /usr/bin/ganesha.nfsd -F -L /tmp/ganesha.log -f ${GANESHA_CONFIGFILE}
+exec /usr/bin/ganesha.nfsd -F -L ${GANESHA_LOGFILE} -f ${GANESHA_CONFIGFILE}
